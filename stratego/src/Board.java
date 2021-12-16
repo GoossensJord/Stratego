39b71c5c601c0f46f97765d7a897806a438e28ee
MichielBrys <brys.michiel99@gmail.com>
2021-12-16 11:50:03 +0100

    Improved board class Groundwork player class

--- stratego/src/Board.java
+++ stratego/src/Board.java
@@ -1,6 +1,6 @@
 public class Board {
-    public int boardHeight = 30; //minimum = 20 increments of 10 only
-    public int boardWidth = 30; //minimum = 20 increments of 10 only
+    public int boardHeight = 20; //minimum = 20 increments of 10 only
+    public int boardWidth = 20; //minimum = 20 increments of 10 only
     private final int SQUARE_ARRAY_WIDTH = Math.abs((boardWidth + 1) / 2);
     private final int SQUARE_ARRAY_HEIGHT = Math.abs((boardHeight + 1) / 6);
 
@@ -9,7 +9,7 @@ public class Board {
     char[][] temporaryReplacementForSquaresTeamA = new char[SQUARE_ARRAY_HEIGHT][SQUARE_ARRAY_WIDTH];
     char[][] temporaryReplacementForSquaresTeamB = new char[SQUARE_ARRAY_HEIGHT][SQUARE_ARRAY_WIDTH];
 
-
+    //if piece selected highlight moveable squares
     public Board() {
 
     }
@@ -26,10 +26,21 @@ public class Board {
 
 
     public void makeBoard() {
-        int counterTeamA = 0;
-        int counterTeamB = 0;
-        System.out.println("Welcome to Stratego!");
+
+        int counterHorizontalIndex = 0;
+        int counterVerticalIndex = 0;
+        System.out.println("    Welcome to Stratego!");
         for (int i = 0; i < boardHeight + 1; i++) {
+            if (i % 2 != 0) {
+                if (counterVerticalIndex < 10) {
+                    System.out.print(counterVerticalIndex++ +"\t");
+                }
+                else System.out.print(counterVerticalIndex++ + "\t");
+
+            }
+            else System.out.print("\t");
+
+
             for (int j = 0; j < boardWidth + 1; j++) {
                 if (i % 2 == 0) {
                     System.out.print("-");
@@ -42,9 +53,18 @@ public class Board {
 
                 else System.out.print(temporaryReplacementForSquaresTeamB[i / 7][j / 2]);
             }
+
             System.out.println();
 
 
+        }
+        System.out.print("    ");
+        for (int i = 0; i < (boardWidth) / 2; i++) {
+            if (counterHorizontalIndex < 10) {
+                System.out.print(" " + counterHorizontalIndex++);
+            }
+            else
+                System.out.print(counterHorizontalIndex++);
         }
 
 
---
+++ stratego/src/Player.java
@@ -0,0 +1,12 @@
+public class Player {
+    String name;
+
+
+
+    Player(String name){
+        this.name = name;
+    }
+    public void play(Board board){
+
+    }
+}
