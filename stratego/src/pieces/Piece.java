7f34ddcfc1f58e64b46d0ccfbaabf0856aad1ab4
GoossensJord <goossensjord99@gmail.com>
2021-12-17 11:01:51 +0100

    Copied files removed
    schijtonedrive

--- stratego/src/pieces/Piece.java
+++ stratego/src/pieces/Piece.java
@@ -1,6 +1,5 @@
 package pieces;
 
-import board.Board;
 import board.Player;
 
 import java.util.ArrayList;
@@ -15,13 +14,13 @@ public class Piece {
     List<int[]> moveableSquares;
     Player player;
 
-    public Piece(Rank r,Player player) {
+    public Piece(Rank r, Player player) {
         this.rank = r;
         this.position = new int[2];
         this.deadOrAlive = true;
         this.isMoveable = true;
         this.canAttack = true;
-        moveableSquares = new ArrayList<int[]>();
+        moveableSquares = new ArrayList<>();
         this.player = player;
     }
 
@@ -30,33 +29,50 @@ public class Piece {
         this.deadOrAlive = true;
     }
 
+    public void setPosition(int[] position) {
+        this.position = position;
+    }
+
     public void move() {
-        System.out.println("Where would you like to move");
+        System.out.println("\nWhere would you like to move?");
 
         int[] pos1 = {this.position[0], this.position[1] + 1};
         int[] pos2 = {this.position[0], this.position[1] - 1};
         int[] pos3 = {this.position[0] + 1, this.position[1]};
         int[] pos4 = {this.position[0] - 1, this.position[1]};
 
+
         moveableSquares.add(pos1);
         moveableSquares.add(pos2);
         moveableSquares.add(pos3);
         moveableSquares.add(pos4);
+        System.out.println("Choose one of the following squares.");
+        for (int i = 0; i < moveableSquares.size(); i++) {
+            if (notOutOfBoundsOrOccupied(moveableSquares.get(i)) == null) moveableSquares.remove(moveableSquares.get(i));
+            else{
+                for (int j = 0; j < moveableSquares.get(i).length; j++) {
+                    System.out.print(moveableSquares.get(i)[j]);
+                }
+                System.out.println();
 
-
-
-    }
-    boolean isOccupied = false;
-    public boolean notOutOfBounds(int[] posarr){
-        boolean available = false;
-        if(!isOccupied) {
-            for (int i = 0; i < posarr.length; i++) {
-                if (posarr[i] > 0 && posarr[i] < player.getBoard().getSQUARE_ARRAY_HEIGHT() && posarr[i] < player.getBoard().getSQUARE_ARRAY_WIDTH())
-                    available = true;
-                else available = false;
-                System.out.println("xd");
+                //System.out.println(moveableSquares.get(i));
             }
         }
-        return available;
+
+    }
+
+    boolean isOccupied = false;
+
+    public int[] notOutOfBoundsOrOccupied(int[] posarr) {
+        if (!isOccupied) {
+            for (int i = 0; i < posarr.length; i++) {
+                if (posarr[i] > 0 && posarr[i] < player.getBoard().getSQUARE_ARRAY_HEIGHT() &&
+                        posarr[i] < player.getBoard().getSQUARE_ARRAY_WIDTH()) {
+                    return posarr;
+                }
+//                System.out.println("xd");
+            }
+        }
+        return null;
     }
 }
--- stratego/src/pieces/Scout-LAPTOP-GUE79NDO.java
+++
@@ -1,8 +0,0 @@
-package pieces;
-//moves any distance horizontal & vertical (no leaping over)
-public class Scout extends Piece{
-//    @Override
-//    public void move() {
-//
-//    }
-}
