62709718e69ddc14bcd1ed50e1c15ca88b0596a3
GoossensJord <61225354+GoossensJord@users.noreply.github.com>
2021-12-18 14:13:22 +0100

    marshall exception attack added.

--- stratego/src/pieces/Spy.java
+++ stratego/src/pieces/Spy.java
@@ -1,4 +1,9 @@
 package pieces;
 //defeats the Marshal if he attacks first
 public class Spy extends Piece{
+    @Override
+    public Piece attack(Piece piece) {
+        if(piece instanceof Marshal) ((Marshal) piece).captured = true;
+        return this;
+    }
 }
