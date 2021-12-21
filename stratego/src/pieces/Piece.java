feb888f5b1e1a90d70c2977313ea39c75a40fcb0
GoossensJord <goossensjord99@gmail.com>
2021-12-21 16:41:02 +0100

    Basic move on piece done.

--- stratego/src/pieces/Piece.java
+++ stratego/src/pieces/Piece.java
@@ -14,7 +14,8 @@ public class Piece {
     boolean isMoveable;
     boolean canAttack;
     protected List<int[]> moveableSquares;
-    private Square square;
+    protected int x;
+    protected int y;
 
     public Piece(Rank r, Player player, int x, int y) {
 
@@ -23,66 +24,50 @@ public class Piece {
         this.isMoveable = true;
         this.canAttack = true;
         this.moveableSquares = new ArrayList<>();
-        this.square =
+
     }
 
     public Piece() {
     }
 
-
-
-    public void setPosition(int[] position) {
-        this.position = position;
-    }
-
-    public void makeMoves(){
+    public List<int[]> getMoves(){
         System.out.println("\nWhere would you like to move?");
 
-        int[] pos1 = {this.position[0], this.position[1] + 1};
-        int[] pos2 = {this.position[0], this.position[1] - 1};
-        int[] pos3 = {this.position[0] + 1, this.position[1]};
-        int[] pos4 = {this.position[0] - 1, this.position[1]};
+        int[] pos1 = {this.x, this.y+1};
+        int[] pos2 = {this.x, this.y - 1};
+        int[] pos3 = {this.x + 1, this.y};
+        int[] pos4 = {this.x - 1, this.y};
 
         moveableSquares.add(pos1);
         moveableSquares.add(pos2);
         moveableSquares.add(pos3);
         moveableSquares.add(pos4);
-    }
 
-    public List<int[]> getMoveableSquares() {
+        return this.moveableSquares;
+    }
+// Removes non moveable squares from array, implementation moved/has to move to board.
+//    public List<int[]> getMoveableSquares() {
+//
+//        for (int i = 0; i < moveableSquares.size(); i++) {
+//            if (outOfBoundsOrOccupied(moveableSquares.get(i))) moveableSquares.set(i,null);
+//            else{
+//                for (int j = 0; j < moveableSquares.get(i).length; j++) {
+//                    System.out.print(moveableSquares.get(i)[j]);
+//                }
+//                System.out.println();
+//            }
+//            return moveableSquares;
+//        }
+//    }
 
-        for (int i = 0; i < moveableSquares.size(); i++) {
-            if (this.outOfBoundsOrOccupied(moveableSquares.get(i))) moveableSquares.set(i,null);
-            else{
-                for (int j = 0; j < moveableSquares.get(i).length; j++) {
-                    System.out.print(moveableSquares.get(i)[j]);
-                }
-                System.out.println();
-            }
-            return moveableSquares;
-        }
-    }
-    public void move(){
-        makeMoves();
-        getMoveableSquares();
-    }
-
-    public Piece attack(Piece piece){
-        if(piece.getRankPower() < this.getRankPower()) return this;
-        else return piece;
-    }
+//    public Piece attack(Piece piece){
+//        if(piece.getRankPower() < this.getRankPower()) return this;
+//        else return piece;
+//    }
 
 
     public Rank getRank() {
         return rank;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
     }
 
     public String toString(){
