d731dcf22c76b844aa1a8c0a2a3d2bc90c1f9efc
GoossensJord <goossensjord99@gmail.com>
2022-02-15 12:11:17 +0100

    Update Scout.java

--- stratego/src/pieces/Scout.java
+++ stratego/src/pieces/Scout.java
@@ -11,7 +11,67 @@ public class Scout extends Piece {
 
     public Scout(Rank scout, Player player, int x, int y) {
         super(Rank.SCOUT, player, x, y);
+<<<<<<< Updated upstream
         movableSquares = new ArrayList<List<int[]>>();
         for (int i = 0; i < 4; i++) movableSquares.add(new ArrayList<int[]>());
+=======
+    }
+
+    public List<int[]> allMoves() {
+        if (firstMove) {
+            this.firstMove = false;
+            return firstmove();
+        } else {
+            List<int[]> allmoves = new ArrayList<>(xMoveable());
+            allmoves.addAll(yMoveable());
+
+            return allmoves;
+        }
+
+    }
+
+    private List<int[]> firstmove() {
+
+        List<int[]> startmoves = new ArrayList<>();
+        if (this.getPlayer().getId() == 1) {
+            startmoves.add(new int[]{this.x + 1, this.y});
+            startmoves.add(new int[]{this.x + 2, this.y});
+        } else {
+            startmoves.add(new int[]{this.x - 1, this.y});
+            startmoves.add(new int[]{this.x - 2, this.y});
+        }
+        return startmoves;
+    }
+
+    public List<int[]> yMoveable() {
+        List<int[]> moves = new ArrayList<>();
+        int xval = this.x;
+
+        for (int i = xval+1; i < 10; i++) {
+            if (this.getPlayer().getBoard().spaceAvailable(i, this.y)) moves.add(new int[]{i, this.y});
+            else break;
+        }
+        for (int i = xval - 1; i >= 0; i--) {
+            if (this.getPlayer().getBoard().spaceAvailable(i, this.y)) moves.add(new int[]{i, this.y});
+            else break;
+        }
+        return moves;
+ 
+    }
+
+    public List<int[]> xMoveable() {
+        List<int[]> moves = new ArrayList<>();
+        int yval = this.y;
+
+        for (int i = yval+1; i < 10; i++) {
+            if (this.getPlayer().getBoard().spaceAvailable(this.x, i)) moves.add(new int[]{this.x, i});
+            else break;
+        }
+        for (int i = yval - 1; i >= 0; i--) {
+            if (this.getPlayer().getBoard().spaceAvailable(this.x, i)) moves.add(new int[]{this.x, i});
+            else break;
+        }
+        return moves;
+>>>>>>> Stashed changes
     }
 }
