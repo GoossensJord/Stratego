487643c4a91d2a7ae0303ea165787c0781154e72
GoossensJord <goossensjord99@gmail.com>
2022-02-25 11:08:35 +0100

    beautyfixes

--- StrategoJavaFX/src/main/java/be/kdg/applicatienaam/gameview/GamePresenter.java
+++ StrategoJavaFX/src/main/java/be/kdg/applicatienaam/gameview/GamePresenter.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GamePresenter {
@@ -31,12 +32,12 @@ public class GamePresenter {
             int y = (int) (e.getY() / 78);
             int x = 9 - (int) (e.getX() / 78);
 
-            List<int[]> moveArr = model.getMoves(model.choosePiece((int)e.getX() / 78, (int) e.getY() / 78));
+            List<int[]> moveArr = model.getMoves(model.choosePiece((int) e.getX() / 78, (int) e.getY() / 78));
             //view.lightUp(coordConverter(moveArr));
             coordConverter(moveArr);
-            System.out.println("first convert \t"+x + " " + y);
-            System.out.println("chosen coords on gridpan\t"+ e.getX() / 78 + " " + e.getY() / 78);
-            System.out.println("chosen piece \t" + model.choosePiece((int)e.getX() / 78, (int) e.getY() / 78).toString());
+            System.out.println("first convert \t" + x + " " + y);
+            System.out.println("chosen coords on gridpan\t" + e.getX() / 78 + " " + e.getY() / 78);
+            System.out.println("chosen piece \t" + model.choosePiece((int) e.getX() / 78, (int) e.getY() / 78).toString());
             //view.lightUp(moveArr);
         });
     }
@@ -46,9 +47,9 @@ public class GamePresenter {
     }
 
     private void fillBoardWithImages() {
-        for (int i = 10; i > 0; i--) {
+        for (int i = 9; i >= 0; i--) {
             for (int j = 0; j < 10; j++) {
-                if (model.getBoard()[i][j].getPiece() == null ) view.setPosition("empty square", i, j);
+                if (model.getBoard()[i][j].getPiece() == null) view.setPosition("empty square", i, j);
                 else view.setPosition(model.getBoard()[i][j].getPiece().toString(), i, j);
                 //else view.setPicture(model.getBoard()[i][j].getPiece().getImage(), i, j);
             }
@@ -56,11 +57,14 @@ public class GamePresenter {
     }
 
     private List<int[]> coordConverter(List<int[]> moveArr) {
+        List<int[]> temparr = new ArrayList<>();
         for (int i = 0; i < moveArr.size(); i++) {
-            moveArr.get(i)[0] = 9-moveArr.get(i)[0];
-            System.out.println("Movearr "+moveArr.get(i)[0] + " " + moveArr.get(i)[1]);
+
+            int temp = 9 - moveArr.get(i)[0];
+            temparr.add(new int[]{temp, moveArr.get(i)[1]});
+            System.out.println("Movearr " + temparr.get(i)[0] + " " + temparr.get(i)[1]);
         }
-        return moveArr;
+        return temparr;
     }
 
     public void addWindowEventHandlers() {
